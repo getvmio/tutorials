b681f16f1cce3df166ab4ac96ab66ff612ae686f
Haibo <nomango@qq.com>
2024-05-29 17:44:00 +0800

    ci: update create new tutorial workflow

--- scripts/insert-tutorial.ts
+++ scripts/insert-tutorial.ts
@@ -1,46 +1,11 @@
 import { parseArgs } from "@std/cli/parse-args";
 import { parseIssue } from "./parse-issue.ts";
-import { parseTutorials } from './parse-tutorials.ts';
-import { dumpTutorials } from './dump-tutorials.ts';
+import { parseTutorials } from "./parse-tutorials.ts";
+import { dumpTutorials } from "./dump-tutorials.ts";
 
 const parsedArgs = parseArgs(Deno.args);
 
-const body = `### Title
-
-Good first issue
-
-### Url
-
-https://goodfirstissue.dev
-
-### Description of this tutorial
-
-Good First Issue curates easy pickings from popular open-source projects, and helps you make your first contribution to open-source.
-Hello.
-
-Hello World
-
-### Authors
-
-DeepSource
-
-### Category
-
-Technical Tutorials
-
-### Tags
-
-Ubuntu, Github
-
-### Recommend a Template
-
-vnc-ubuntu:2204
-
-### Terms
-
-- [X] I agree`;
-
-const tutorial = parseIssue(body);
+const tutorial = parseIssue(Deno.env.get("ISSUE_BODY")!);
 console.log(tutorial);
 
 const tutorials = await parseTutorials(parsedArgs.file);
